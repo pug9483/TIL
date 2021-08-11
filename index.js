7456abb3a38d1e9365363a6d57ad95dbebc7a770
pug9483 <pug9483>
2021-08-12 00:46:51 +0900

    modularity

--- index.js
+++
@@ -1,7 +0,0 @@
-// import express from "express";
-// const express = require("express");
-
-// //express 애플리케이션 만들기
-// const app = express();
-
-console.log("by")
--- src/server.js
+++ src/server.js
@@ -1,23 +1,23 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+
 
 const PORT = 4000;
 
 const app = express();
-
+const logger = morgan("dev");
 //application 설정 구간 -> application을 만들고, 외부 접속을 listen하는 사이에 작성해야 됨
 //application에게 get request에 응답하는 방법 등등을 가르침
 
-const handleHome = (req, res) => {
-    return res.send("/ page");
-}
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
-const handleLogin = (req, res) => {
-    return res.send("Login here.");
-}
-app.get("/", handleHome);
-app.get("/login", handleLogin)
 
 const handleListening = () =>
- console.log(`Server listening on port http://localhost:${PORT}`);
+ console.log(`✅ Server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening)
