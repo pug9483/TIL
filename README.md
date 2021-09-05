4c62256087a985064efc01d7d4021747239d22b7
pug9483 <67894738+pug9483@users.noreply.github.com>
2021-09-05 23:07:12 +0900

    url_and_pug

--- README.md
+++ README.md
@@ -5,11 +5,12 @@
 /login -> Login
 /search -> Search
 
-/users/edit -> Edit user
-/users/delete -> Delete user
+/users/:id -> See User
+/users/logout -> Log Out
+/users/edit -> Edit My Profile
+/users/delete -> Delete My Profile
 
-/videos/watch -> Watch Video
-/videos/edit -> Edit Video
-/vidoes/delete -> Delete Video
-/videos/comments -> Comment on a video
-/videos/comments/delete -> Delete A
+/videos/:id -> See Video
+/videos/:id/edit -> Edit Video
+/vidoes/:id/delete -> Delete Video
+/videos/upload -> Upload Video
--- src/server.js
+++ src/server.js
@@ -4,20 +4,22 @@ import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-
 const PORT = 4000;
 
 const app = express();
-const logger = morgan("dev");
+// const logger = morgan("dev");
+
 //application 설정 구간 -> application을 만들고, 외부 접속을 listen하는 사이에 작성해야 됨
 //application에게 get request에 응답하는 방법 등등을 가르침
-
+console.log(process.cwd());
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+// app.use(logger);
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-
 const handleListening = () =>
- console.log(`✅ Server listening on port http://localhost:${PORT}`);
+  console.log(`✅ Server listening on port http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening)
+app.listen(PORT, handleListening);
