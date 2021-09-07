59627100557b1d8f96b8704e58734569121777a1
pug9483 <67894738+pug9483@users.noreply.github.com>
2021-09-08 01:47:23 +0900

    partials_and_extends_with_pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,6 @@
-export const trending = (req, res) => res.render("home");
-export const see = (req, res) => {
-  console.log(req.params);
-  res.send("Watch");
-};
-export const edit = (req, res) => res.send("Edit");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
