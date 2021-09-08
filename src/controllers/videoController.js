fae86e54d9d52f0c51479a02e6fc6f3c1cd62c67
pug9483 <67894738+pug9483@users.noreply.github.com>
2021-09-08 23:52:56 +0900

    iterator_and_mixin

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,36 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+const fakeUser = {
+  username: "pug9483",
+  loggedIn: false,
+};
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 2,
+    },
+    {
+      title: "Third video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 3,
+    },
+  ];
+  res.render("home", { pageTitle: "Home", fakeUser, videos });
+};
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search");
