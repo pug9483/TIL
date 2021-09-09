821e971a371db13c9bfb2c834b5539993a7efaf7
pug9483 <67894738+pug9483@users.noreply.github.com>
2021-09-10 01:47:24 +0900

    relativeAddress_and_editProfile

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,38 +1,42 @@
-const fakeUser = {
-  username: "pug9483",
-  loggedIn: false,
-};
+const videos = [
+  {
+    title: "First video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "Second video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Second video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 2,
-    },
-    {
-      title: "Third video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 3,
-    },
-  ];
-  res.render("home", { pageTitle: "Home", fakeUser, videos });
+  res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", {
+    pageTitle: `Watching ${video.title}`,
+    video,
+  });
+};
+export const edit = (req, res) => res.render("edit", { videos });
+export const search = (req, res) => res.send("search", { videos });
+export const upload = (req, res) => res.send("Upload", { videos });
+export const deleteVideo = (req, res) => res.send("Delete Video", { videos });
--- src/server.js
+++ src/server.js
@@ -11,7 +11,6 @@ const app = express();
 
 //application 설정 구간 -> application을 만들고, 외부 접속을 listen하는 사이에 작성해야 됨
 //application에게 get request에 응답하는 방법 등등을 가르침
-console.log(process.cwd());
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 // app.use(logger);
