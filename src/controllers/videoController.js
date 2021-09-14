4fc4b7e065abdc1864645f0863425255fa2df44e
pug9483 <67894738+pug9483@users.noreply.github.com>
2021-09-15 01:32:45 +0900

    install_mongodb

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -48,8 +48,22 @@ export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
   videos[id - 1].title = title;
+  console.log(req.body);
   return res.redirect(`/videos/${id}`);
 };
-export const search = (req, res) => res.send("search", { videos });
-export const upload = (req, res) => res.send("Upload", { videos });
-export const deleteVideo = (req, res) => res.send("Delete Video", { videos });
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+export const postUpload = (req, res) => {
+  const newVideo = {
+    title: req.body.title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
+};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,17 +3,14 @@ import {
   watch,
   getEdit,
   postEdit,
-  deleteVideo,
-  upload,
+  getUpload,
+  postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)", watch);
-// videoRouter.get("/:id(\\d+)/edit", getEdit);
-// videoRouter.post("/:id(\\d+)/edit", postEdit);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
