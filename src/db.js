483c48841e3509c19ed284510239181815fba32a
pug9483 <67894738+pug9483@users.noreply.github.com>
2021-09-29 03:26:49 +0900

    connect db

--- src/db.js
+++ src/db.js
@@ -5,5 +5,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/wetube");
 const db = mongoose.connection;
 
 const handleOpen = () => console.log("✅ Connected to DB");
-db.on("error", (error) => console.log("❌ DB Error", error));
+const handleError = () => console.log("❌ DB Error");
+
+db.on("error", handleError);
 db.once("open", handleOpen);
