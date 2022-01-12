885e90a97e5158e523536b44dc66c351ea8bd8a4
pug9483 <67894738+pug9483@users.noreply.github.com>
2022-01-12 17:27:39 +0900

    Jdbc 사용

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,15 +1,24 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 // command + p : 인자로 들어가야 할 변수의 자료형을 알 수 있음
 
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     // 스프링 빈에 등록할 것이란 뜻
     @Bean
@@ -20,6 +29,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
