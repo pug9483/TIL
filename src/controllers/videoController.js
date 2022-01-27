3f814d0790e78fc9ead62ee59d2a1d06f12db3e2
pug9483 <67894738+pug9483@users.noreply.github.com>
2022-01-27 23:53:14 +0900

    login

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -24,7 +24,7 @@ export const getEdit = async (req, res) => {
   const video = await Video.findById(id);
   //누군가가 존재하지 않는 영상 id로 request를 보낼 수 있음
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   return res.render("edit", { pageTitle: `Edit ${video.title}`, video });
 };
@@ -34,7 +34,7 @@ export const postEdit = async (req, res) => {
   const { title, description, hashtags } = req.body;
   const video = await Video.exists({ _id: id });
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   await Video.findByIdAndUpdate(id, {
     title,
@@ -58,7 +58,7 @@ export const postUpload = async (req, res) => {
     });
     return res.redirect("/");
   } catch (error) {
-    return res.render("upload", {
+    return res.status(400).render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
