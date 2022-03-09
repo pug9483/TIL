66056c952acf9762626b22725d82571179369ef4
pug9483 <67894738+pug9483@users.noreply.github.com>
2022-03-10 01:24:38 +0900

    github authentication

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,18 @@
 import express from "express";
-import {edit, remove, logout, see} from "../controllers/userController";
+import {
+  edit,
+  logout,
+  see,
+  startGithubLogin,
+  finishGithubLogin,
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get(":id", see);
 userRouter.get("/logout", logout);
-userRouter.get("/edit", edit)
-userRouter.get("/remove", remove);
+userRouter.get("/edit", edit);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 
 export default userRouter;
