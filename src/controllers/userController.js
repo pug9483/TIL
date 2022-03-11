6937a4c86e4072e06f45fdb36e019af701044011
pug9483 <67894738+pug9483@users.noreply.github.com>
2022-03-11 23:18:46 +0900

    populate를 사용하여 profile수정

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import Video from "../models/Video";
 import fetch from "node-fetch";
 import bcrypt from "bcrypt";
 import { application } from "express";
@@ -216,7 +217,8 @@ export const postChangePassword = async (req, res) => {
 
 export const see = async (req, res) => {
   const { id } = req.params;
-  const user = await User.findById(id);
+  const user = await User.findById(id).populate("videos");
+  console.log(user);
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found" });
   }
