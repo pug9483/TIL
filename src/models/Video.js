7694a89c0c295f776d7acd32d7c527b1dd603258
pug9483 <67894738+pug9483@users.noreply.github.com>
2022-03-11 16:27:34 +0900

    video upload

--- src/models/Video.js
+++ src/models/Video.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true, maxlength: 80 },
-  description: { type: String, required: true, trim: true, minlength: 20 },
+  fileUrl: { type: String, required: true },
+  description: { type: String, required: true, trim: true },
   createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String, trim: true }],
   meta: {
