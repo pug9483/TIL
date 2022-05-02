12667efb7c96d6bc368939cb1e6f0f34f5549123
pug9483 <67894738+pug9483@users.noreply.github.com>
2022-05-03 00:47:44 +0900

    AutoAppConfig

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
-// 저장소
+@Component
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
